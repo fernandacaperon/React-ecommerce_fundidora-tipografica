23d4a1d831612e228e34275b18b3e51fb6b65308
fernandacaperon <fernandacaperon@gmail.con>
2022-05-10 08:01:38 -0300

    desafio Item collection

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,6 +2,7 @@ import ItemList from './ItemList';
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { collection,  getDocs } from 'firebase/firestore';
+import { firestoreFetch } from '../utils/firestoreFetch';
 import db from '../utils/firebaseConfig';
 
 const ItemListContainer = () => {
@@ -9,20 +10,19 @@ const ItemListContainer = () => {
     const [tipografias, setTipografias] = useState ([]);
     const { idCategory } = useParams ();
 
-    useEffect (()=> {
-        const fetchFromFirestore = async () => {
-            const querySnapshot = await getDocs(collection(db, "users"));
-            const dataFromFirestore = querySnapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            })); 
-            return dataFromFirestore;
-            }
-            fetchFromFirestore ()
-            .then (result => setTipografias (result))
-            .catch (err => console.log (err)) 
-            ;
+    useEffect(() => {
+        firestoreFetch(idCategory)
+            .then(res => setTipografias(res))
+            .catch(err => console.log(err));
+        
         }, [idCategory]);
+
+        useEffect(() => {
+            return (() => {
+            setTipografias([]);
+            })
+    }, []);
+
 
     return (
         <>
